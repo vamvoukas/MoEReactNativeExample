73c1266df475cc9d7840f908a53defe3d4269e4c
Chengappa C D <chengappa@moengage.com>
2017-10-13 18:04:28 +0530

    AppDelegate changes for notification observers

--- ios/MoEReactNativeExample/AppDelegate.m
+++ ios/MoEReactNativeExample/AppDelegate.m
@@ -8,10 +8,9 @@
  */
 
 #import "AppDelegate.h"
-
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
+#import "MOEHelperConstants.h"
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -31,7 +30,38 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  //This is optional, Only do it if you need the callbacks for the notification related methods
+  
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidReceiveRemoteNotification:) name:MoEngage_Notification_Received_Notification object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidRegisterForRemoteNotification:) name:MoEngage_Notification_Registered_Notification object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidRegisterForUserSettings:) name:MoEngage_Notification_UserSettings_Registered_Notification object:nil];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidFailToRegisterForRemoteNotification:) name:MoEngage_Notification_Registration_Failed_Notification object:nil];
+  
   return YES;
+}
+
+
+#pragma mark- MoEngage Notification Observer methods
+
+-(void)applicationDidReceiveRemoteNotification:(NSNotification *)notification{
+  NSLog(@"Notification payload :  %@",notification.userInfo);
+}
+
+
+-(void)applicationDidRegisterForRemoteNotification:(NSNotification *)notification{
+  NSData* deviceToken = notification.userInfo[MoEngage_Device_Token_Key];
+  NSLog(@"notif info :  %@ And deviceToken : %@",notification, deviceToken);
+}
+
+-(void)applicationDidRegisterForUserSettings:(NSNotification *)notification{
+  
+  UIUserNotificationSettings *settings = notification.userInfo[MoEngage_Notification_Settings_Key];
+  NSLog(@"settings info :  %@ And settings : %@",notification,settings);
+}
+
+-(void)applicationDidFailToRegisterForRemoteNotification:(NSNotification *)notification{
+  NSLog(@"settings info :  %@",notification);
 }
 
 @end
